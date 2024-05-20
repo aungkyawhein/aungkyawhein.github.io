c61038a20463e0f01b8d6f5aa2f758abe1221fc8
aungkyawhein <akh.infotech@gmail.com>
2024-05-20 22:56:55 +1200

    Update header

---
+++ src/lib/components/Header2.svelte
@@ -0,0 +1,12 @@
+<script>
+  import logo from "$lib/assets/logo-transparent.png";
+  import Container from "./Container.svelte";
+</script>
+
+<header class="py-4">
+  <Container>
+    <nav class="flex justify-between items-center">
+      <img src={logo} alt="Logo" width="60" height="60" />
+    </nav>
+  </Container>
+</header>
